5f9b0307331aa3328f695c71d63223112e8b4002
Ayon Saha <64322407+ayon420-coder@users.noreply.github.com>
2026-01-21 04:08:13 +0600

    Enhance createAccount with DataAccess and ID generation
    Added DataAccess functionality and account ID generation.

--- Fahad Store/UI/createAccount.cs
+++ Fahad Store/UI/createAccount.cs
@@ -1,13 +1,21 @@
-﻿using System;
+using System;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
-using System.Drawing;
 
 namespace Fahad_Store.UI
 {
     public partial class createAccount : Form
     {
         private dashBoard db;
+        private DataAccess da;
+
+        public DataAccess Da
+        {
+            get { return da; }
+            set { da = value; }
+        }
         public dashBoard Db1
         {
             get { return db; }
@@ -24,19 +32,65 @@ namespace Fahad_Store.UI
         public createAccount()
         {
             InitializeComponent();
-        }
+            this.Da = new DataAccess();
 
-        
+        }
 
         public createAccount(logIn ln)
         {
             InitializeComponent();
+            this.Da = new DataAccess();
             this.Ln = ln;
         }
 
-        private void crtAccBtn_Click(object sender, EventArgs e)
+        private void createAccount_Load(object sender, EventArgs e)
         {
 
+        }
+        public string generateID()
+        {
+            int currentID = 1001;
+            try
+            {
+                string query = "SELECT MAX(CAST(SUBSTRING(CUSTOMER_ID, CHARINDEX('-', CUSTOMER_ID) + 1, LEN(customer_ID)) AS INT)) AS MaxCustID FROM Customers;";
+                DataTable tb = new DataTable();
+                tb = Da.ExecuteQueryTable(query);
+                if (tb.Rows.Count > 0 && tb.Rows[0][0] != DBNull.Value)
+                {
+                    currentID = Convert.ToInt32(tb.Rows[0][0]) + 1;
+                }
+                Da.Sqlcon.Close();
+                return Convert.ToString(currentID);
+
+            }
+            catch (Exception E)
+            {
+                return Convert.ToString(currentID);
+            }
+        }
+
+
+        private void crtAccBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Are you really want to create an account?", "Confirmation",MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (r == DialogResult.No)
+            {
+                return;
+            }
+            try
+            {
+                string customerID = "C-" + generateID();
+                string userName = firstNameTb.Text + " " + lastNameTb.Text;
+                string query = "INSERT INTO Customers (customer_id, Username, gender, date_of_birth, address, contact, password) " +
+                               "VALUES ('" + customerID + "', '" + userName + "', '" + genderBox.Text + "', '" +
+                               dateOfBirthPicker.Value + "', '" + addressTb.Text + "', '" + contactTb.Text + "', '" + passwordTb.Text + "')";
+                Da.ExecuteQuery(query);
+                MessageBox.Show("Account created successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void backButton_Click(object sender, EventArgs e)
