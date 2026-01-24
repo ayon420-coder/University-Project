e9b9ce6096dbda23ed073e06365c6d7670e2c53b
Ayon Saha <64322407+ayon420-coder@users.noreply.github.com>
2026-01-25 04:17:36 +0600

    Session file uploaded in Necessities folder.

---
+++ Fahad Store/Necessities/Session.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahad_Store.Necessities
+{
+    public static class Session
+    {
+        public static string UserId { get; private set; }
+        public static string Username { get; private set; }
+        public static bool IsLoggedIn => !string.IsNullOrEmpty(UserId);
+
+        public static void Login(string userId, string username)
+        {
+            UserId = userId;
+            Username = username;
+        }
+
+        public static void Logout()
+        {
+            UserId = null;
+            Username = null;
+        }
+    }
+
+}
